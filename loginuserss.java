0a1d2ed5e4fd50bc5736c2cfb80cb766ea694281
krishna441 <krishnachaithanya84@gmail.com>
2020-08-30 17:44:41 +0530

    remove junk

--- loginuserss.java
+++
@@ -1,8 +0,0 @@
-public class login
-{
-public static void main(string args[])
-{
-int a = 10;
-	system.out.printin("Hello world");
-}
-}
--- payment.java
+++
@@ -1,8 +0,0 @@
-Public class login 
-{
-Public static void main(string args[])
-{
-Int a = 10;
-System.out.printin(“hello world”);
-}
-}
